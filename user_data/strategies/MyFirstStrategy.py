dbb2045fce641e65c5ff6f9c22feb7e622000e42
SajawalLiaqat1 <63193004+SajawalLiaqat1@users.noreply.github.com>
2026-04-30 10:29:42 +0500

    Update MyFirstStrategy.py

--- user_data/strategies/MyFirstStrategy.py
+++ user_data/strategies/MyFirstStrategy.py
@@ -1,1 +1,39 @@
+from freqtrade.strategy import IStrategy
+import talib.abstract as ta
+import pandas as pd
 
+class MyFirstStrategy(IStrategy):
+
+    stoploss = -0.05
+    minimal_roi = {
+        "0":  0.03,
+        "30": 0.02,
+        "60": 0.01
+    }
+    trailing_stop = True
+    timeframe = '15m'
+
+    def populate_indicators(self, df, metadata):
+        df['rsi'] = ta.RSI(df['close'], timeperiod=14)
+        macd = ta.MACD(df['close'])
+        df['macd']   = macd['macd']
+        df['signal'] = macd['macdsignal']
+        bb = ta.BBANDS(df['close'], timeperiod=20)
+        df['bb_upper'] = bb['upperband']
+        df['bb_lower'] = bb['lowerband']
+        return df
+
+    def populate_entry_trend(self, df, metadata):
+        df.loc[
+            (df['rsi'] < 35) &
+            (df['macd'] > df['signal']) &
+            (df['close'] < df['bb_lower']),
+            'enter_long'] = 1
+        return df
+
+    def populate_exit_trend(self, df, metadata):
+        df.loc[
+            (df['rsi'] > 65) &
+            (df['macd'] < df['signal']),
+            'exit_long'] = 1
+        return df
